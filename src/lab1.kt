9563ebb29ef68d66ad65c06f75276f1a8efca9fb
Анастасия <shmelca98@gmail.com>
2024-01-17 21:46:50 +0300

    change SDK, fix lab1

--- src/lab1.kt
+++ src/lab1.kt
@@ -1,9 +1,11 @@
+// Сгенерировать массив целых чисел случайным образом, вывести его на экран, отсортировать его, и снова вывести на экран.
+
 import kotlin.random.Random
 fun main() {
     // создание массива с рандомными числами
-    val randomValues = Array<Int>(10){Random.nextInt()}
+    val randomValues = Array(10){Random.nextInt()}
     var result = ""
-    randomValues.forEach(){
+    randomValues.forEach{
         result += "$it,"
     }
     println("[${result.substring(0,result.count()-1)}]")
@@ -11,7 +13,7 @@ fun main() {
     // сортировка по возрастанию
     randomValues.sort()
     var result2 = ""
-    randomValues.forEach(){
+    randomValues.forEach{
         result2 += "$it,"
     }
     println("[${result2.substring(0,result2.count()-1)}]")
