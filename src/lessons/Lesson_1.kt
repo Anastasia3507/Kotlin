9292178ca7e5729bec4f4543548be8b2488c82ce
Анастасия <shmelca98@gmail.com>
2024-01-23 22:45:24 +0300

    change lesson_2

--- src/lessons/Lesson_1.kt
+++ src/lessons/Lesson_1.kt
@@ -17,18 +17,18 @@ fun main(){
     println("Мой возраст $myAge лет. Через 10 лет, мне будет $myAgeInTenYear лет, с момента моего рождения пройдет $daysPassed дней.")
 
     // задание 3
-    val bein_1 = 8.0
-    val bein_2 = 6.0
-    val areaOfTriangle = area(bein_1, bein_2)
-    val perimeterOfTriangle = perimeter(bein_1, bein_2)
+    val beinAB = 8.0
+    val beinCB = 6.0
+    val areaOfTriangle = area(beinAB, beinCB)
+    val perimeterOfTriangle = perimeter(beinAB, beinCB)
     println("Площадь прямоугольного треугольника равна $areaOfTriangle, периметр прямоугольного треугольника равен $perimeterOfTriangle.")
 }
 
-fun area(bein_1: Double, bein_2: Double) =
-    (bein_1 * bein_2) / 2
+fun area(bein1: Double, bein2: Double) =
+    (bein1 * bein2) / 2
 
 fun hypotenuse(beinAB: Double, beinCB: Double) =
     sqrt(beinAB.pow(2.0) + beinCB.pow(2.0))
 
-fun perimeter(bein_1: Double, bein_2: Double) =
-    bein_1 + bein_2 + hypotenuse(beinCB = bein_1, beinAB = bein_2)
+fun perimeter(bein1: Double, bein2: Double) =
+    bein1 + bein2 + hypotenuse(beinCB = bein1, beinAB = bein2)
