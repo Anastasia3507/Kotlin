e6b0643f139179cf3629e7cbceb59c20f125bbb7
DenLyamin <51235336+DenLyamin@users.noreply.github.com>
2024-02-08 20:13:29 +0300

    Update Furniture.kt

--- src/lab_works/lab3_4/Furniture.kt
+++ src/lab_works/lab3_4/Furniture.kt
@@ -1,5 +1,7 @@
 package lab_works.lab3_4
 
+Если ты хочешь переменные в абстрактных классах, то укажи их как обязательное поле в конструкторе.
+На практике я не видел таких переменных. 
 abstract class Furniture {
     abstract val material: String
     abstract val price: Double
