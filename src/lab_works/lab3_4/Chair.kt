6aef261087c0c0e481f8644a27ae1571d3adf94d
anastasiya.shmeleva <anastasiya.shmeleva@coolclever.ru>
2024-02-12 17:47:06 +0300

    add fun equals and hashCode in lab3_4

--- src/lab_works/lab3_4/Chair.kt
+++ src/lab_works/lab3_4/Chair.kt
@@ -4,5 +4,19 @@ class Chair(private val material: String, private val backOfChair: Boolean) : Fu
     private val priceChair: Double
         get() = if (material == "wood") 900.0 else 700.0
 
-    override fun getPrice(): Double = if (backOfChair) priceChair + 100.0 else priceChair
+    override fun getPrice(): Double = if (backOfChair) priceChair + 200.0 else priceChair
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Chair
+
+        return material == other.material
+    }
+
+    override fun hashCode(): Int {
+        return material.hashCode()
+    }
+
+
 }
