b4fe15646ae00a76d2e3eecb01ac4f519a2e8f09
Анастасия <shmelca98@gmail.com>
2024-02-13 21:12:11 +0300

    changed extensions in lesson 5

--- src/lab_works/lab3_4/Chair.kt
+++ src/lab_works/lab3_4/Chair.kt
@@ -4,7 +4,8 @@ class Chair(private val material: String, private val backOfChair: Boolean) : Fu
     private val priceChair: Double
         get() = if (material == "wood") 900.0 else 700.0
 
-    override fun getPrice(): Double = if (backOfChair) priceChair + 200.0 else priceChair
+    override fun getPrice(): Double = if (backOfChair) priceChair + 100.0 else priceChair
+
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
